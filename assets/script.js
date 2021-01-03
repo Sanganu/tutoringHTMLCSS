dc2054305b3eb950ec4ad1464c641f85bfe3918f
Sanganu <kppkmsanu@gmail.com>
2021-01-03 09:13:04 -0700

    Rearrange and remove duplicate functionality

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,12 @@ let clockStart = setInterval(displayCount,1000)
 let countHTML = document.getElementById("count");
 let test1HTML = document.getElementById("test1");
 let test2HTML = document.getElementById("test2");
-let logHTML = document.getElementById("logentry");
+// let logHTML = document.getElementById("logentry");
 let perfHTML = document.getElementById("performanceLog")
 let stopHTML = document.getElementById("stoptimer");
 let resetCountHTML = document.getElementById("resetCount");
 let clearLogHTML = document.getElementById("clearLog")
+let restartHTML = document.getElementById("restarttimer")
 
 let counter = 0;
 let timer=0;
@@ -23,7 +24,7 @@ countHTML.addEventListener("click",function(){
     test1HTML.innerText = counter
 })
 
-logHTML.addEventListener("click",saveToLocalStorage)
+// logHTML.addEventListener("click",saveToLocalStorage)
 
 function saveToLocalStorage(){
     let performance = JSON.parse(localStorage.getItem("performance")) || []
@@ -32,17 +33,19 @@ function saveToLocalStorage(){
         timer:timer
     })
     console.log("Log",performance)
-    localStorage.setItem("performance",JSON.stringify(performance))
+    localStorage.setItem("performance",JSON.stringify(performance));
+    counter = 0;
+    timer = 0;
     retriveLocalStorage()
 }
 
 function retriveLocalStorage(){
     let performance = JSON.parse(localStorage.getItem("performance")) || []
-    let htmlString = "<ul>"
+    let htmlString = ""
     htmlString += performance.map((dataentry) => {
-        return(`<li>Counts:${dataentry.reps} Time:${dataentry.timer}</li>`)
+        return(`<tr><td>${dataentry.reps}</td><td>${dataentry.timer}</td></tr>`)
     })
-    htmlString += "</ul>"
+    // htmlString += "</ul>"
     console.log(htmlString)
     perfHTML.innerHTML = htmlString
 }
@@ -53,6 +56,7 @@ function stopTimer(){
 
 
 resetCountHTML.addEventListener("click",function(){
+    saveToLocalStorage()
     counter = 0;
     timer = 0;
     test1HTML.innerText = counter
@@ -62,6 +66,11 @@ resetCountHTML.addEventListener("click",function(){
 clearLogHTML.addEventListener("click",function(){
     localStorage.removeItem("performance");
     perfHTML.innerHTML =""
+})
+
+
+restartHTML.addEventListener("click",function(){
+    clockStart = setInterval(displayCount,1000)
 })
 
 
